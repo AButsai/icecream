72d80d1015052199e8455ee0db352d1b8abb880e
AButsai <butsai@ukr.net>
2021-12-05 16:45:09 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './sass/main.scss';
+
+(() => {
+  const menuBtnRef = document.querySelector('.menu-button');
+  const mobileMenuRef = document.querySelector('.header__menu');
+  const mobileMenuOverflow = document.querySelector('body');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+    mobileMenuOverflow.classList.toggle('menu-open');
+  });
+})();
