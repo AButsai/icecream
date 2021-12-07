18b303a364f4eab543c659962b51e81d7e5941f9
AButsai <butsai@ukr.net>
2021-12-07 17:50:19 +0200

    feaute/buy-modal

--- src/index.js
+++ src/index.js
@@ -15,26 +15,48 @@ import './sass/main.scss';
   }
 })();
 
+const bodyToggleClass = document.querySelector('body');
+
+// мобильное меню
 const openMenu = document.querySelector('.open-menu');
 const closeMenu = document.querySelector('.close-menu');
-const openModalHero = document.querySelector('[open-hero-modal]');
-const closeModalHero = document.querySelector('[close-hero-modal]');
+
 const headerMenu = document.querySelector('.header__menu');
-const bodyToggleClass = document.querySelector('body');
-const heroModal = document.querySelector('.hero__backdrop');
-const heroModal = document.querySelector('.header__backdrop');
 
 const openMenuMobile = openMenu.addEventListener('click', toggleMenu);
 const closeMenuMobile = closeMenu.addEventListener('click', toggleMenu);
-const openModal = openModalHero.addEventListener('click', toggleModalHero);
-const closeModal = closeModalHero.addEventListener('click', toggleModalHero);
 
 function toggleMenu(e) {
   headerMenu.classList.toggle('is-open');
   bodyToggleClass.classList.toggle('is-open');
 }
 
+// модальное окно в секции Hero
+const openModalHero = document.querySelector('[open-hero-modal]');
+const closeModalHero = document.querySelector('[close-hero-modal]');
+
+const heroModal = document.querySelector('.hero__backdrop');
+
+const openModal = openModalHero.addEventListener('click', toggleModalHero);
+const closeModal = closeModalHero.addEventListener('click', toggleModalHero);
+
 function toggleModalHero(e) {
   heroModal.classList.toggle('is-hidden');
+  bodyToggleClass.classList.toggle('is-open');
+}
+
+// модальное окно в Header
+const openModalHeader1 = document.querySelector('[open-buy-modal-1]');
+const openModalHeader = document.querySelector('[open-buy-modal]');
+const closeModalHeader = document.querySelector('[close-buy-modal]');
+
+const headerModal = document.querySelector('.header__backdrop');
+
+const openModalBuy = openModalHeader.addEventListener('click', toggleModalBuy);
+const openModalBuy1 = openModalHeader1.addEventListener('click', toggleModalBuy);
+const closeModalBuy = closeModalHeader.addEventListener('click', toggleModalBuy);
+
+function toggleModalBuy(e) {
+  headerModal.classList.toggle('is-hidden');
   bodyToggleClass.classList.toggle('is-open');
 }
